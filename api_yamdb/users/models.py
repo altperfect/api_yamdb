5065154f653b831d54dd6110f1471fd461d253b4
altperfect <32578434+altperfect@users.noreply.github.com>
2023-02-14 09:20:32 +0500

    explicit imports

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from .utils import generate_confirmation_code
+from users.utils import generate_confirmation_code
 
 
 class User(AbstractUser):
