b2ae729c49b6d5149ee77be57e1320d9eda6c045
altperfect <32578434+altperfect@users.noreply.github.com>
2023-02-14 07:36:03 +0500

    more explicit versioning

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,10 +3,10 @@ from rest_framework.routers import DefaultRouter
 
 from .views import UserViewSet
 
-router = DefaultRouter()
-router.register('users', UserViewSet, basename='users')
+v1_router = DefaultRouter()
+v1_router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('v1/auth/', include('users.urls')),
-    path("v1/", include(router.urls)),
+    path("v1/", include(v1_router.urls)),
 ]
