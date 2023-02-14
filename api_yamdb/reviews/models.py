420051376c34653f0d3e80b6a48053e35a1e44b3
Anastasia <dark.exiles.666@mail.ru>
2023-02-14 10:30:13 +0500

    corrections

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,9 @@
-from django.db import models
 from django.conf import settings
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
-from users.models import User
 from titles.models import Title
+from users.models import User
 
 
 class Review(models.Model):
